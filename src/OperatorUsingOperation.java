7f65c6f567c0b1e389d940c6f3c8d14105ec0e2a
papri221003 <bhpapri03@gmail.com>
2025-09-06 20:50:02 +0530

    Modified

--- src/OperatorUsingOperation.java
+++ src/OperatorUsingOperation.java
@@ -1,2 +1,12 @@
 public class OperatorUsingOperation {
+    public static void main(String[] args) {
+        int num1=13;
+        int num2=7;
+        int result=num1 & num2;
+        int result1=num1 | num2;
+        int result3=num1 ^ num2;
+        System.out.println("AND "+result);
+        System.out.println("OR "+result1);
+        System.out.println("XOR "+result3);
+    }
 }
