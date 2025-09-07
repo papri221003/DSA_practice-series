4403a83ad3969dcf2eb2d74f6040c3ba8dc07e7d
papri221003 <bhpapri03@gmail.com>
2025-09-07 08:04:25 +0530

    Modified

--- src/Check_set_ithBit.java
+++ src/Check_set_ithBit.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Check_set_ithBit {
-    public static boolean CheckIthBit(int num,int i)
+    public static boolean CheckIthBit_leftShift(int num,int i)
     {
         int n=1<<i;
         if((num & n)!=0){
@@ -9,14 +9,29 @@ public class Check_set_ithBit {
         else
             return false;
     }
+
+    public static boolean CheckIthBit_rightShift(int num,int i)
+    {
+        int n=num>>i;
+        if((n & 1)!=0){
+            return true;
+        }
+        else
+            return false;
+    }
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the number: ");
         int num=sc.nextInt();
         System.out.println("Enter the ith possition: ");
         int i=sc.nextInt();
-        boolean res=CheckIthBit(num,i);
+        boolean res=CheckIthBit_leftShift(num,i);
         System.out.println(res);
+
+        System.out.println();
+        boolean res1=CheckIthBit_rightShift(num,i);
+        System.out.println(res1);
 
     }
 }
