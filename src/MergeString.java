13ab9f212c1e08a1556505ddbf8fc564c132451e
papri221003 <bhpapri03@gmail.com>
2025-10-19 16:27:40 +0530

    inital commit

--- src/MergeString.java
+++ src/MergeString.java
@@ -4,21 +4,23 @@ public class MergeString {
     public static String mergeAlternately(String word1, String word2) {
         StringBuilder sb=new StringBuilder();
         int i=0;
-        while(i<word1.length() && i<word2.length())
+        int j=0;
+        while(i<word1.length() && j<word2.length())
         {
             sb.append(word1.charAt(i));
-            sb.append(word2.charAt(i));
+            sb.append(word2.charAt(j));
             i++;
+            j++;
         }
         while(i<word1.length())
         {
             sb.append(word1.charAt(i));
             i++;
         }
-        while(i<word2.length())
+        while(j<word2.length())
         {
-            sb.append(word2.charAt(i));
-            i++;
+            sb.append(word2.charAt(j));
+            j++;
         }
         return sb.toString();
 
