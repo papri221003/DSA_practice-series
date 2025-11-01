efef0484baaa136ea295788fcf3e37d817270ac6
papri221003 <bhpapri03@gmail.com>
2025-11-01 19:20:08 +0530

    initial commit

--- src/Isomorphic_String.java
+++ src/Isomorphic_String.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Isomorphic_String {
+    public static boolean isIsomorphic(String s, String t) {
+        HashMap<Character,Character>map=new HashMap<>();
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            if(!map.containsKey(c) && map.containsValue(t.charAt(i)))
+            {
+                return false;
+            }
+            else if(map.containsKey(c) && map.get(c)!=t.charAt(i))
+            {
+                return false;
+            }
+            else
+            {
+                map.put(c,t.charAt(i));
+            }
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String t=sc.nextLine();
+
+        System.out.println(isIsomorphic(s,t));
+    }
 }
