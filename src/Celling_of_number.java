e02b693d04c87303384cf3e70a2cdf33500b6755
papri221003 <bhpapri03@gmail.com>
2025-11-03 19:35:55 +0530

    initial commit

--- src/Celling_of_number.java
+++ src/Celling_of_number.java
@@ -1,31 +1,29 @@
 public class Celling_of_number {
-    public static int celling(int arr[],int target)
+    public static int celling_number(int num[],int target)
     {
         int start=0;
-        int end=arr.length-1;
+        int end=num.length-1;
         while(start<=end)
         {
             int mid=(start+end)/2;
-            if(arr[mid]==target)
+            if(num[mid]==target)
             {
-                return arr[mid];
-            }
-            else if(arr[mid]<target)
-            {
-                start=mid+1;
-            }
-            else
+                return num[mid];
+            } else if (num[mid]>target)
             {
                 end=mid-1;
             }
+            else {
+                start=mid+1;
+            }
         }
-        return arr[start];
+        return num[start];
     }
     public static void main(String[] args) {
-        int arr[]={2, 3, 5, 9, 14, 16, 18};
-        int target=6;
-        int result=celling(arr,target);
-        System.out.println(result);
+        int num[]={2,3,5,9,14,16,18};
+        int target=15;
+
+        System.out.println(celling_number(num,target));
 
     }
 }
