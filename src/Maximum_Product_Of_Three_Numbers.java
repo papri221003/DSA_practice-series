369ef105ccfeee2ab74aa96293a3bdf079dca1b8
papri221003 <bhpapri03@gmail.com>
2025-11-04 21:44:50 +0530

    initial commit

---
+++ src/Maximum_Product_Of_Three_Numbers.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Maximum_Product_Of_Three_Numbers {
+    public static int maximumProduct(int[] nums) {
+
+        Arrays.sort(nums);
+        int n=nums.length;
+        int product1=nums[n-1]*nums[n-2]*nums[n-3];
+        int product2=nums[0]*nums[1]*nums[n-1];
+
+        return Math.max(product1,product2);
+    }
+
+    public static void main(String[] args) {
+        int nums[]={3,-2,-8,4,1};
+        System.out.println(maximumProduct(nums));
+    }
+}
