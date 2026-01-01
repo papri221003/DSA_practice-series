5ccb2d8952805dbb998d84dcf5bb3de290ed0a08
papri221003 <bhpapri03@gmail.com>
2026-01-01 12:35:08 +0530

    initial commit

---
+++ src/Valid_Parenthesis.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class Valid_Parenthesis {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char c = stack.pop();
+                if ((c == '(' && ch == ')') || (c == '{' && ch == '}') || (c == '[' && ch == ']')) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "{[({})]}";
+        System.out.println(isValid(s));
+
+    }
+}
+
